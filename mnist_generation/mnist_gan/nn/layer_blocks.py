b23bc52fbab19b0ab939f86571d687d759099517
baptisteeeeeeee <bjuran2001@gmail.com>
2024-03-16 18:40:39 +0100

    [mnist_generation] added activation parameter and leaky relu in gan

--- mnist_generation/mnist_gan/nn/layer_blocks.py
+++ mnist_generation/mnist_gan/nn/layer_blocks.py
@@ -4,27 +4,30 @@ from keras.layers import DepthwiseConv2D, Dense, Reshape
 
 
 def dense_block(units: int,
-                do_relu: bool=True,
                 do_bn: bool=True,
                 dropouts: float=None,
                 target_shape: tuple=None,
+                activation: str | Activation='relu',
                 name:str=None):
     if name is not None:
         dense_name = f'{name}_dense'
-        relu_name = f'{name}_relu'
+        act_name = f'{name}_act'
         bn_name = f'{name}_bn'
         dropout_name = f'{name}_dropout'
     else:
         dense_name = None
-        relu_name = None
+        act_name = None
         bn_name = None
         dropout_name = None
     def ret(x: tf.Tensor) -> tf.Tensor:
         x = Dense(units, use_bias=not do_bn, name=dense_name)(x)
         if do_bn:
             x = BatchNormalization(name=bn_name)(x)
-        if do_relu:
-            x = Activation('relu', name=relu_name)(x)
+        if activation is not None:
+            if isinstance(activation, str):
+                x = Activation(activation, name=act_name)(x)
+            else:
+                x = activation(x)
         if dropouts is not None:
             x = Dropout(dropouts, name=dropout_name)(x)
         if target_shape is not None:
@@ -35,7 +38,7 @@ def dense_block(units: int,
 def convolution_block(filters: int, 
                       filter_shape: tuple[int, int] | int, 
                       do_bn: bool=True, 
-                      do_relu: bool=True,
+                      activation: str | Activation='relu',
                       dropouts: float=None, 
                       dilations: tuple[int, int] | int=(1, 1), 
                       strides: tuple[int, int] | int=(1, 1),
@@ -43,12 +46,12 @@ def convolution_block(filters: int,
                       name: str=None):
     if name is not None:
         conv_name = f'{name}_conv'
-        relu_name = f'{name}_bn'
-        bn_name = f'{name}_relu'
+        act_name = f'{name}_act'
+        bn_name = f'{name}_bn'
         dropout_name = f'{name}_dropout'
     else:
         conv_name = None
-        relu_name = None
+        act_name = None
         bn_name = None
         dropout_name = None
     def ret(x: tf.Tensor) -> tf.Tensor:
@@ -57,8 +60,11 @@ def convolution_block(filters: int,
                    use_bias=not do_bn, name=conv_name)(x)
         if do_bn:
             x = BatchNormalization(name=bn_name)(x)
-        if do_relu:
-            x = Activation('relu', name=relu_name)(x)
+        if activation is not None:
+            if isinstance(activation, str):
+                x = Activation(activation, name=act_name)(x)
+            elif isinstance(activation, Activation):
+                x = activation(x)
         if dropouts is not None:
             x = Dropout(dropouts, name=dropout_name)(x)
         return x
@@ -67,22 +73,22 @@ def convolution_block(filters: int,
 def residual_block(filters: int, 
                    filter_shape: tuple[int, int] | int, 
                    do_bn: bool=True, 
-                   do_relu: bool=True,
                    dropouts: float=None, 
                    dilations: tuple[int, int] | int=(1, 1), 
                    strides: tuple[int, int] | int=(1, 1),
                    padding: str='same',
+                   activation: str | Activation='relu',
                    name: str=None):
     if name is not None:
         conv_name = f'{name}_conv'
         add_name = f'{name}_add'
-        relu_name = f'{name}_bn'
-        bn_name = f'{name}_relu'
+        act_name = f'{name}_act'
+        bn_name = f'{name}_bn'
         dropout_name = f'{name}_dropout'
     else:
         conv_name = None
         add_name=None
-        relu_name = None
+        act_name = None
         bn_name = None
         dropout_name = None
     def ret(x: tf.Tensor) -> tf.Tensor:
@@ -92,8 +98,11 @@ def residual_block(filters: int,
         if do_bn:
             x_ = BatchNormalization(name=bn_name)(x)
         x = Add(name=add_name)((x, x_))
-        if do_relu:
-            x = Activation('relu', name=relu_name)(x)
+        if activation is not None:
+            if isinstance(activation, str):
+                x = Activation(activation, name=act_name)(x)
+            elif isinstance(activation, Activation):
+                x = activation(x)
         if dropouts is not None:
             x = Dropout(dropouts, name=dropout_name)(x)
         return x
@@ -101,21 +110,21 @@ def residual_block(filters: int,
 
 def depthwise_block(filter_shape: tuple[int, int] | int, 
                     do_bn: bool=True, 
-                    do_relu: bool=True,
                     dropouts: float=None, 
                     dilations: tuple[int, int] | int=(1, 1), 
                     strides: tuple[int, int] | int=(1, 1),
                     depth_multiplier: int=1,
                     padding: str='same',
+                    activation: str | Activation='relu',
                     name: str=None):
     if name is not None:
         dw_name = f'{name}_dw'
-        relu_name = f'{name}_bn'
-        bn_name = f'{name}_relu'
+        act_name = f'{name}_act'
+        bn_name = f'{name}_bn'
         dropout_name = f'{name}_dropout'
     else:
         dw_name = None
-        relu_name = None
+        act_name = None
         bn_name = None
         dropout_name = None
     def ret(x: tf.Tensor) -> tf.Tensor:
@@ -125,8 +134,11 @@ def depthwise_block(filter_shape: tuple[int, int] | int,
                    name=dw_name)(x)
         if do_bn:
             x = BatchNormalization(name=bn_name)(x)
-        if do_relu:
-            x = Activation('relu', name=relu_name)(x)
+        if activation is not None:
+            if isinstance(activation, str):
+                x = Activation(activation, name=act_name)(x)
+            elif isinstance(activation, Activation):
+                x = activation(x)
         if dropouts is not None:
             x = Dropout(dropouts, dropout_name)(x)
         return x
@@ -135,11 +147,11 @@ def depthwise_block(filter_shape: tuple[int, int] | int,
 def depthwise_separable_convoltion_block(filters: int, 
                                          filter_shape: tuple[int, int] | int, 
                                          do_bn: bool=True, 
-                                         do_relu: bool=True,
                                          dropouts: float=None, 
                                          dilations: tuple[int, int] | int=(1, 1), 
                                          strides: tuple[int, int] | int=(1, 1),
                                          padding: str='same',
+                                         activation: str | Activation='relu',
                                          depth_multiplier: int=1,
                                          name: str=None):
     if name is not None:
@@ -151,7 +163,7 @@ def depthwise_separable_convoltion_block(filters: int,
     def ret(x: tf.Tensor) -> tf.Tensor:
         x = depthwise_block(filter_shape=filter_shape,
                             do_bn=do_bn,
-                            do_relu=do_relu,
+                            activation=activation,
                             strides=strides,
                             dilations=dilations,
                             depth_multiplier=depth_multiplier,
@@ -159,7 +171,7 @@ def depthwise_separable_convoltion_block(filters: int,
         x = convolution_block(filters=filters, 
                               filter_shape=(1, 1),
                               do_bn=do_bn,
-                              do_relu=do_relu,
+                              activation=activation,
                               dropouts=dropouts,
                               padding=padding,
                               name=conv_name)(x)
@@ -169,11 +181,11 @@ def depthwise_separable_convoltion_block(filters: int,
 def spacewise_separable_convoltion_block(filters: int, 
                                          filter_shape: tuple[int, int], 
                                          do_bn: bool=True, 
-                                         do_relu: bool=True,
                                          dropouts: float=None, 
                                          dilations: tuple[int, int]=(1, 1), 
                                          strides: tuple[int, int]=(1, 1),
                                          padding: str='same',
+                                         activation: str | Activation='relu',
                                          rank: int=4,
                                          name: str=None):
     if name is not None:
@@ -186,7 +198,7 @@ def spacewise_separable_convoltion_block(filters: int,
         x = convolution_block(filters=rank,
                               filter_shape=(filter_shape[0], 1),
                               do_bn=do_bn,
-                              do_relu=do_relu,
+                              activation=activation,
                               padding=padding,
                               strides=(strides[0], 1),
                               dilations=(dilations[0], 1),
@@ -194,7 +206,7 @@ def spacewise_separable_convoltion_block(filters: int,
         x = convolution_block(filters=filters, 
                               filter_shape=(1, filter_shape[1]),
                               do_bn=do_bn,
-                              do_relu=do_relu,
+                              activation=activation,
                               dropouts=dropouts,
                               padding=padding,
                               strides=(1, strides[1]),
@@ -206,7 +218,7 @@ def spacewise_separable_convoltion_block(filters: int,
 def bottleneck_convolution_block(filters: int, 
                                  filter_shape: tuple[int, int] | int, 
                                  do_bn: bool=True, 
-                                 do_relu: bool=True,
+                                 activation: str | Activation='relu',
                                  dropouts: float=None, 
                                  dilations: tuple[int, int] | int=(1, 1), 
                                  strides: tuple[int, int] | int=(1, 1),
@@ -225,12 +237,12 @@ def bottleneck_convolution_block(filters: int,
         x = convolution_block(filters=filters//reduction,
                               filter_shape=(1, 1),
                               do_bn=do_bn,
-                              do_relu=do_relu,
+                              activation=activation,
                               name=conv1_name)(x)
         x = convolution_block(filters=filters//reduction,
                               filter_shape=filter_shape,
                               do_bn=do_bn,
-                              do_relu=do_relu,
+                              activation=activation,
                               dilations=dilations,
                               strides=strides,
                               padding=padding,
@@ -238,7 +250,7 @@ def bottleneck_convolution_block(filters: int,
         x = convolution_block(filters=filters,
                               filter_shape=filter_shape,
                               do_bn=do_bn,
-                              do_relu=do_relu,
+                              activation=activation,
                               dropouts=dropouts,
                               name=conv3_name)(x)
         return x
@@ -248,7 +260,7 @@ def bottleneck_convolution_block(filters: int,
 def xnet_depthwise_separable_convoltion_block(filters: int, 
                                               filter_shape: tuple[int, int] | int, 
                                               do_bn: bool=True, 
-                                              do_relu: bool=True,
+                                              activation: str | Activation='relu',
                                               dropouts: float=None, 
                                               dilations: tuple[int, int] | int=(1, 1), 
                                               strides: tuple[int, int] | int=(1, 1),
@@ -265,12 +277,12 @@ def xnet_depthwise_separable_convoltion_block(filters: int,
         x = convolution_block(filters=filters, 
                               filter_shape=(1, 1),
                               do_bn=do_bn,
-                              do_relu=do_relu,
+                              activation=activation,
                               padding=padding,
                               namae=conv_name)(x)
         x = depthwise_block(filter_shape=filter_shape,
                             do_bn=do_bn,
-                            do_relu=do_relu,
+                            activation=activation,
                             dilations=dilations,
                             strides=strides,
                             dropouts=dropouts,
--- mnist_generation/mnist_gan/nn/models.py
+++ mnist_generation/mnist_gan/nn/models.py
@@ -9,37 +9,36 @@ def create_generator() -> Model:
 
     x = dense_block(units=7*7*32, do_bn=False,
                     target_shape=(7, 7, 32), name='latent_embedding')(x1)
-    x = convolution_block(32, (3, 3))(x)
-    x = depthwise_separable_convoltion_block(32, (3, 3))(x)
+    x = convolution_block(32, (3, 3), activation='leaky_relu')(x)
+    x = depthwise_separable_convoltion_block(32, (3, 3), activation='leaky_relu')(x)
 
     x = UpSampling2D()(x)
-    x = convolution_block(64, (3, 3))(x)
-    x = depthwise_separable_convoltion_block(64, (3, 3))(x)
+    x = convolution_block(64, (3, 3), activation='leaky_relu')(x)
+    x = depthwise_separable_convoltion_block(64, (3, 3), activation='leaky_relu')(x)
 
     x = UpSampling2D()(x)
-    x = convolution_block(128, (3, 3))(x)
-    x = depthwise_separable_convoltion_block(128, (3, 3))(x)
-    x = convolution_block(128, (3, 3))(x)
-    x = convolution_block(1, (1, 1), do_relu=False)(x)
-    x = Activation('tanh')(x)
+    x = convolution_block(128, (3, 3), activation='leaky_relu')(x)
+    x = depthwise_separable_convoltion_block(128, (3, 3), activation='leaky_relu')(x)
+    x = convolution_block(128, (3, 3), activation='leaky_relu')(x)
+    x = convolution_block(1, (1, 1), activation='tanh')(x)
 
     return Model(inputs=input_latent, outputs=x, name='mnist_generator')
 
 
 def create_discriminator() -> Model:
     input_image = x = Input((28, 28, 1), name='input_image')
-    x = convolution_block(32, (1, 1))(x)
-    x = depthwise_separable_convoltion_block(32, (3, 3))(x)
-    x = convolution_block(32, (2, 2), strides=(2, 2))(x)
+    x = convolution_block(32, (1, 1), activation='leaky_relu')(x)
+    x = depthwise_separable_convoltion_block(32, (3, 3), activation='leaky_relu')(x)
+    x = convolution_block(32, (2, 2), strides=(2, 2), activation='leaky_relu')(x)
     
-    x = depthwise_separable_convoltion_block(64, (3, 3))(x)
-    x = convolution_block(64, (2, 2), strides=(2, 2))(x)
+    x = depthwise_separable_convoltion_block(64, (3, 3), activation='leaky_relu')(x)
+    x = convolution_block(64, (2, 2), strides=(2, 2), activation='leaky_relu')(x)
 
-    x = depthwise_separable_convoltion_block(128, (3, 3))(x)
-    x = convolution_block(128, (2, 2))(x)
+    x = depthwise_separable_convoltion_block(128, (3, 3), activation='leaky_relu')(x)
+    x = convolution_block(128, (2, 2), activation='leaky_relu')(x)
 
     x = GlobalAveragePooling2D()(x)
 
-    x = dense_block(1, do_relu=False, do_bn=False)(x)
+    x = dense_block(1, do_bn=False, activation=None)(x)
 
     return Model(inputs=input_image, outputs=x, name='mnist_discriminator')
